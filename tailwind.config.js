20533d7bca459eee0f4e8496264b75be6a396bb6
Nicholas Berlette <nick@berlette.com>
2021-11-10 14:54:28 -0800

    ✏️  fix tailwind dakrmode typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: media, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
