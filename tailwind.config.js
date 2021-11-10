9062158a33b5e6a40009a0fe268fb1ed740d56d0
Nicholas Berlette <nick@berlette.com>
2021-11-10 14:36:21 -0800

    💄 update tailwind to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: media, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
@@ -35,5 +34,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }
