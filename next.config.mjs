dccc6dd1bd0df161b5f146068b1a2c77a89772b6
Nicholas Berlette <nick@berlette.com>
2021-11-19 14:11:29 -0800

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,31 +2,63 @@ import withPreact from 'next-plugin-preact';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Next.js 12 Rust Compiler (SWC): uncomment this if needed to disable
-  // swcMinify: true,
+  // v12 Rust Compiler (SWC): uncomment this if needed to disable
+  // swcMinify: false,
   
-  // https://nextjs.org/docs/basic-features/image-optimization#domains
+  // https://nextjs.org/docs/basic-features/image-optimization
   images: {
+    // https://nextjs.org/docs/basic-features/image-optimization#domains
     domains: [
-      'cdn.jsdelivr.net',
-      'unpkg.com',
       'icns.vercel.app',
       'icns.imgix.net',
       'raw.githubusercontent.com',
       'user-images.githubusercontent.com'
     ],
+    
+    // enable AVIF + WEBP
+    formats: ['image/avif', 'image/webp'],
+    
+    // breakpoints (default from nextjs.org)
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    
+    // image sizes to generate:
+    // largest value MUST be smaller than the *smallest* deviceSize
+    imageSizes: [/*16, */32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 120
   },
 
-  // https://nextjs.org/docs/api-reference/next.config.js/rewrites
+  /**
+   * rewrites • must be an asynchronous function that returns either:
+   * - an array of Rewrite type Objects:
+   *   @returns [
+   *       { source: string, destination: string, has?: RewriteHas[] },
+   *       ...
+   *     ]
+   * - or an object with 3 (optional) arrays as properties:
+   *   @returns {
+   *       beforeFiles: [] -- applied *before* file system routing
+   *       afterFiles: []  -- applied *after*  file system routing
+   *       fallback: []    -- fallback rewrites for error resolution
+   *     }
+   * 
+   * @see https://nextjs.org/docs/api-reference/next.config.js/rewrites
+   */
   rewrites: async () => {
     return [
+      // standard page rewrite, passing :slug parameter to the destination
       {
         source: '/photo-:slug/?',
 	destination: '/photos/:slug/',
       },
+      // using Next Rewrites to mask our Prismic CMS URL
       {
 	source: '/cms/:path*',
 	destination: 'https://malanna.prismic.io/:path',
+      },
+      // using Next Rewrites to mask our github repository
+      {
+        source: '/github/:path*',
+	destination: 'https://github.com/nberlette/malanna/:path'
       }
     ]
   },
@@ -34,13 +66,16 @@ const nextConfig = {
   // https://nextjs.org/docs/api-reference/next.config.js/redirects
   redirects: async () => {
     return [
+      // permanent redirect from /repo/ to our GitHub Repository Readme
       {
         source: '/repo/?',
-	destination: 'https://github.com/nberlette/malanna',
+	destination: 'https://github.com/nberlette/malanna#readme',
 	permanent: true
       },
     ]
   },
+
+  // insert additional next configurations here
 };
 
 export default withPreact(nextConfig);
