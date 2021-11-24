cd541c574f4e2f781041601573a20e9212da5004
Nicholas Berlette <nick@berlette.com>
2021-11-23 23:47:08 -0800

    fix ts component props

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,8 +1,8 @@
 import cn from 'classnames';
 
-export default function Container({ children }: { children?: any } = {}) {
+export default function Container({ children, className }: { children?: any, className?: any } = {}) {
   return (
-      <div className={cn("container mx-auto px-5")}>
+      <div className={cn("container mx-auto px-5", className)}>
         {children}
       </div>
   );
