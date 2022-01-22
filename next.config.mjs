f929b9ac69bdbeb17843c68dbd0924a6566b7dd0
Nicholas Berlette <nick@berlette.com>
2022-01-22 15:20:12 -0800

    update config and settings, add windicss

--- next.config.mjs
+++ next.config.mjs
@@ -1,65 +1,48 @@
 import withPreact from 'next-plugin-preact'
+import WindiCSSWebpackPlugin from 'windicss-webpack-plugin'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default withPreact({
+  experimental: {
+    optimizeCss: true,
+    optimizeImages: true,
+  },
 
-  // v12 Rust Compiler (SWC) - uncomment to disable
-  // swcMinify: false,
-  
-  // https://nextjs.org/docs/basic-features/image-optimization
   images: {
-
     domains: [
+      'icns.ml',
+      'icns.cf',
       'icns.vercel.app',
       'icns.imgix.net',
+      'iconic.s3.amazonaws.com',
       'images.prismic.io',
+      'malanna.vercel.app',
       'malanna.page',
+      'malanna.prismic.io',
       'static.malanna.page',
       'raw.githubusercontent.com',
       'user-images.githubusercontent.com'
     ],
-    
-    // enable AVIF + WEBP
     formats: ['image/avif', 'image/webp'],
-    
-    // breakpoints (default from nextjs.org)
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    
-    // image sizes to generate:
-    // largest value MUST be smaller than the *smallest* deviceSize
-    imageSizes: [/*16, */32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 120
+    imageSizes: [32, 48, 64, 96, 128, 256, 384, 480],
+    minimumCacheTTL: 600
   },
 
-  /**
-   * rewrites • must be an asynchronous function that returns either:
-   * - an array of Rewrite type Objects:
-   *   @returns [
-   *       { source: string, destination: string, has?: RewriteHas[] },
-   *       ...
-   *     ]
-   * - or an object with 3 (optional) arrays as properties:
-   *   @returns {
-   *       beforeFiles: [] -- applied *before* file system routing
-   *       afterFiles: []  -- applied *after*  file system routing
-   *       fallback: []    -- fallback rewrites for error resolution
-   *     }
-   * 
-   * @see https://nextjs.org/docs/api-reference/next.config.js/rewrites
-   */
   rewrites: async () => {
     return [
-      // standard page rewrite, passing :slug parameter to the destination
       {
-        source: '/photo-:slug',
+        source: '/photo-:slug*',
         destination: '/photos/:slug',
       },
-      // preview feature
       {
-        source: '/preview/:path*',
-        destination: 'https://malanna.page/api/preview/:path',
+        source: '/exit-preview/?:path*',
+        destination: 'api/exit-preview/:path'
       },
-      // using Next Rewrites to mask our github repository
+      {
+        source: '/preview/?:path*',
+        destination: 'api/preview/:path',
+      },
       {
         source: '/github/:path*',
         destination: 'https://github.com/nberlette/malanna/:path'
@@ -67,10 +50,8 @@ const nextConfig = {
     ]
   },
 
-  // https://nextjs.org/docs/api-reference/next.config.js/redirects
   redirects: async () => {
     return [
-      // permanent redirect from /repo/ to our GitHub Repository Readme
       {
         source: '/repo',
         destination: 'https://github.com/nberlette/malanna#readme',
@@ -79,8 +60,9 @@ const nextConfig = {
     ]
   },
 
-  // insert additional next configurations here
-};
+  webpack: (config) => {
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  }
 
-export default withPreact(nextConfig);
-
+});
